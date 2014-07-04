559d2509c29ae7f32d64a5a240e1c5558e9f6584
Edgar Sánchez Grajeda <EdSG@users.noreply.github.com>
2014-07-04 14:56:11 -0700

    3. Classes - Exercise 4

---
+++ src/3-classes/x-3-4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class Foo
+{
+  private:
+
+    int _value;
+
+  public:
+
+  Foo(int value=0):_value(value)
+  {
+  }
+
+  friend std::istream& operator>>(std::istream& in, Foo& f)
+  {
+    return in >> f._value;
+  }
+
+  friend std::ostream& operator<<(std::ostream& out, const Foo& f)
+  {
+    return out << "Foo(" << &f << ":" << f._value << ")";
+  }
+
+};
+
+int main(int argc, char *argv[], char *envp[])
+{
+  Foo f;
+
+  std::cout << f << std::endl;
+
+  std::cout << "Provide Foo with a new value: ";
+  std::cin >> f;
+
+  std::cout << f << std::endl;
+
+  return 0;
+}
+
